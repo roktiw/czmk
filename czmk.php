6824ee4029e1dda3161d67fdba9a848202ddd247
Maciej Jankowski <maciej.artur.jankowski@gmail.com>
2016-10-22 14:34:51 +0200

    poprawkis

--- czmk.php
+++ czmk.php
@@ -1,15 +1,13 @@
 <?php
 include('pass.php');
 include('defaults.php');
-
 try {
-	include('dbconnect.php');
-  $res = $dbh->query('SELECT SUM(val) as sum,(SELECT val FROM votes ORDER BY `date` DESC LIMIT 1) as last_vote from votes WHERE date > NOW() - INTERVAL 1 HOUR');
-  print_r($res->fetchObject());
+  include('dbconnect.php');
+  $res = $dbh->query('SELECT val, date, (SELECT SUM(val) as sum from votes WHERE date > NOW() - INTERVAL 1 HOUR) as sum FROM votes ORDER BY `date` DESC LIMIT 1');
+  echo(json_encode($res->fetchObject(), JSON_PRETTY_PRINT));
   $dbh = null;
 } catch (PDOException $e) {
   print "Error!: " . $e->getMessage() . "<br/>";
   die();
 }
-
 ?>
